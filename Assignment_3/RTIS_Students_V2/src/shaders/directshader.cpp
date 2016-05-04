c79c3a10d14073809a58e4ed3b93bf98258c01ab
Marti Serrallonga <msb_bcn@hotmail.com>
2016-05-04 16:29:56 +0200

    Compute color updated

--- Assignment_3/RTIS_Students_V2/src/shaders/directshader.cpp
+++ Assignment_3/RTIS_Students_V2/src/shaders/directshader.cpp
@@ -8,35 +8,35 @@ DirectShader::DirectShader(Vector3D color_, double maxDist_, Vector3D bgColor_) 
 	color(color_), Shader(bgColor_)
 {}
 
+
 Vector3D DirectShader::computeColor(const Ray &r, const std::vector<Shape*> &objList, const std::vector<PointLightSource> &lsList) const
 {
 	Intersection closestInt;
-	Vector3D color = Vector3D(0,0,0);
-	
+	Vector3D color = Vector3D(0, 0, 0);
+
 	if (Utils::getClosestIntersection(r, objList, closestInt))
 	{
 		Vector3D p = closestInt.itsPoint;
 		Vector3D n = closestInt.normal;
-
 		for (int i = 0; i < lsList.size(); i++)
 		{
-			Vector3D wi = (lsList[i].getPosition() - closestInt.itsPoint).normalized();
-	
-			Ray lightRay(lsList[i].getPosition(), wi);
+			Vector3D wi = (lsList[i].getPosition() - closestInt.itsPoint);
+			double maxT = wi.length();
+			wi = wi.normalized();
 
-			if(dot(wi, n)>0)
+			Ray shadowRay(closestInt.itsPoint, wi);
+			shadowRay.maxT = maxT;
+			if (dot(wi, n)>0)
 			{
-				if (!Utils::hasIntersection(lightRay, objList))
+				if (!Utils::hasIntersection(shadowRay, objList))
 				{
-					Vector3D reflectance = closestInt.shape->getMaterial().getReflectance(n, -r.d, wi);					
+					Vector3D reflectance = closestInt.shape->getMaterial().getReflectance(n, -r.d, wi);
 					color += Utils::multiplyPerCanal(lsList[i].getIntensity(p), reflectance);
 				}
 			}
 		}
-
 		return color;
 	}
 	else
 		return bgColor;
-	
 }
